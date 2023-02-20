adb3fcf2cbdd1404f119445239e1365f182c88ae
Matt Holmstrom <mjholmstr@gmail.com>
2023-02-19 20:05:31 -0600

    Import from sample array and use data for card props

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Card from './Card';
 import 'tachyons';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    </div>
   </React.StrictMode>
 );
 
