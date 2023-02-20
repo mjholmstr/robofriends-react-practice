104580272935e03b9642395e2d53fe2a3dd5ac99
Matt Holmstrom <mjholmstr@gmail.com>
2023-02-19 20:03:46 -0600

    Create array with sample data objects

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Robot 1',
+        username: 'Bret',
+        email: 'bret@robofriends'
+    },
+    {
+        id: 2,
+        name: 'Robot 2',
+        username: 'Kurt',
+        email: 'Kurt@robofriends'
+    },
+    {
+        id: 3,
+        name: 'Robot 3',
+        username: 'Cynthia',
+        email: 'Cynthia@robofriends'
+    },
+    {
+        id: 4,
+        name: 'Robot 4',
+        username: 'Wilfred',
+        email: 'Wilfred@robofriends'
+    },
+    {
+        id: 5,
+        name: 'Robot 5',
+        username: 'Corny',
+        email: 'Corny@robofriends'
+    },
+    {
+        id: 6,
+        name: 'Robot 6',
+        username: 'Megan',
+        email: 'Megan@robofriends'
+    },
+    {
+        id: 7,
+        name: 'Robot 7',
+        username: 'Humphrey',
+        email: 'Humphrey@robofriends'
+    },
+    {
+        id: 8,
+        name: 'Robot 8',
+        username: 'Hank',
+        email: 'Hank@robofriends'
+    },
+    {
+        id: 9,
+        name: 'Robot 9',
+        username: 'Courtney',
+        email: 'Courtney@robofriends'
+    },
+    {
+        id: 10,
+        name: 'Robot 10',
+        username: 'Mandy',
+        email: 'Mandy@robofriends'
+    },
+
+]
