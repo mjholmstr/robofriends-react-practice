3d9091d0743d3473ae1b4071ad51248e422ef8ac
Matt Holmstrom <mjholmstr@gmail.com>
2023-02-19 21:20:46 -0600

    Render App.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CardList from './CardList';
 import 'tachyons';
-import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App />
   </React.StrictMode>
 );
 
