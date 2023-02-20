25456fb171cd826020e7f8152e67545f618e5cc9
Matt Holmstrom <mjholmstr@gmail.com>
2023-02-19 21:19:54 -0600

    Change object names to better test search filter

--- src/robots.js
+++ src/robots.js
@@ -1,61 +1,61 @@
 export const robots = [
     {
         id: 1,
-        name: 'Robot 1',
+        name: 'Bret the Robot',
         username: 'Bret',
         email: 'bret@robofriends'
     },
     {
         id: 2,
-        name: 'Robot 2',
+        name: 'Kurtman',
         username: 'Kurt',
         email: 'Kurt@robofriends'
     },
     {
         id: 3,
-        name: 'Robot 3',
+        name: 'Cynthiaborg',
         username: 'Cynthia',
         email: 'Cynthia@robofriends'
     },
     {
         id: 4,
-        name: 'Robot 4',
+        name: 'Wilfred',
         username: 'Wilfred',
         email: 'Wilfred@robofriends'
     },
     {
         id: 5,
-        name: 'Robot 5',
+        name: 'Scarecrow',
         username: 'Corny',
         email: 'Corny@robofriends'
     },
     {
         id: 6,
-        name: 'Robot 6',
+        name: 'Megbot',
         username: 'Megan',
         email: 'Megan@robofriends'
     },
     {
         id: 7,
-        name: 'Robot 7',
+        name: 'HHRobo',
         username: 'Humphrey',
         email: 'Humphrey@robofriends'
     },
     {
         id: 8,
-        name: 'Robot 8',
+        name: 'Hank the Tank',
         username: 'Hank',
         email: 'Hank@robofriends'
     },
     {
         id: 9,
-        name: 'Robot 9',
+        name: 'Court2343',
         username: 'Courtney',
         email: 'Courtney@robofriends'
     },
     {
         id: 10,
-        name: 'Robot 10',
+        name: 'Mandy',
         username: 'Mandy',
         email: 'Mandy@robofriends'
     },
